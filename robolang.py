0be5171889e56beed1550fa5aeb8c9cb1ec2bcd0
Archan Patkar <archanpatkar@gmail.com>
2018-01-25 03:11:01 +0530

    Update

--- robolang.py
+++ robolang.py
@@ -3,13 +3,10 @@ from Robot import Robot
 from Robot import default
 
 robotlang = metamodel_from_file('robot.tx')
-
 robot_model = robotlang.model_from_file('one.rl')
 robotlang.register_obj_processors({'MoveCommand': default})
 
-
 r1 = Robot()
-
 r1.interpret(robot_model);
 
 input("Program Complete Press Enter to Close");
