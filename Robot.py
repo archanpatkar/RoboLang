cd8cddd5f88fe1f4dd30405c748afc6563af3bc4
Archan Patkar <archanpatkar@gmail.com>
2018-01-25 17:40:26 +0530

    Update

--- Robot.py
+++ Robot.py
@@ -15,43 +15,49 @@ class Robot:
 
     def interpret(self, model):
         for c in model.commands:
-
-            if c.__class__.__name__ == "InitialCommand":
-                self.x = c.x
-                self.y = c.y
-                self.turtle.setpos(self.x,self.y)
-            elif c.__class__.__name__ == "MoveCommand":
-                dir = c.direction
-                print("Going {} for {} step(s).".format(dir, c.steps))
-                if(dir == "up"):
-                    self.x += c.steps * 0
-                    self.y += c.steps * 1
-                    self.turtle.setx(self.x)
-                    self.turtle.sety(self.y)
-                elif(dir == "down"):
-                    self.x += c.steps * 0
-                    self.y += c.steps * -1 
-                    self.turtle.setx(self.x)
-                    self.turtle.sety(self.y)
-                elif(dir == "left"):
-                    self.x += c.steps * -1
-                    self.y += c.steps * 0
-                    self.turtle.setx(self.x)
-                    self.turtle.sety(self.y)
-                elif(dir == "right"):
-                    self.x += c.steps * 1
-                    self.y += c.steps * 0
-                    self.turtle.setx(self.x)
-                    self.turtle.sety(self.y)
-            elif c.__class__.__name__ == "GoToCommand":
-                self.x = c.x
-                self.y = c.y
-                self.turtle.penup()
+            self.single(c)
+            time.sleep(1)
+        
+    def single(self,c):
+        if c.__class__.__name__ == "InitialCommand":
+            self.x = c.x
+            self.y = c.y
+            self.turtle.setpos(self.x,self.y)
+        elif c.__class__.__name__ == "MoveCommand":
+            dir = c.direction
+            print("Going {} for {} step(s).".format(dir, c.steps))
+            if(dir == "up"):
+                self.x += c.steps * 0
+                self.y += c.steps * 1
                 self.turtle.setx(self.x)
                 self.turtle.sety(self.y)
-                self.turtle.pendown()
-            elif c.__class__.__name__ == "CircleCommand":
-                self.turtle.circle(c.radius)
-            elif c.__class__.__name__ == "DotCommand":
-                self.turtle.dot(c.size)
-            time.sleep(1)
+            elif(dir == "down"):
+                self.x += c.steps * 0
+                self.y += c.steps * -1 
+                self.turtle.setx(self.x)
+                self.turtle.sety(self.y)
+            elif(dir == "left"):
+                self.x += c.steps * -1
+                self.y += c.steps * 0
+                self.turtle.setx(self.x)
+                self.turtle.sety(self.y)
+            elif(dir == "right"):
+                self.x += c.steps * 1
+                self.y += c.steps * 0
+                self.turtle.setx(self.x)
+                self.turtle.sety(self.y)
+        elif c.__class__.__name__ == "GoToCommand":
+            self.x = c.x
+            self.y = c.y
+            self.turtle.penup()
+            self.turtle.setx(self.x)
+            self.turtle.sety(self.y)
+            self.turtle.pendown()
+        elif c.__class__.__name__ == "CircleCommand":
+            self.turtle.circle(c.radius)
+        elif c.__class__.__name__ == "DotCommand":
+            self.turtle.dot(c.size)
+        elif c.__class__.__name__ == "RepeatCommand":
+            for times in range(c.times):
+                for command in c.commands:
+                    self.single(command)
